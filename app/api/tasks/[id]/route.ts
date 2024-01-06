80b26d920f438a4daabc637457fe21e7b690d789
Mikelesnr <mwanza.n.m@gmail.com>
2024-01-06 22:45:27 +0200

    crud implemented

---
+++ app/api/tasks/[id]/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/app/utils/connect";
+// import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    // const { userId } = auth();
+    const userId = "user_2aXCP17scwi6xa9kAGE7Ge57Fky";
+    const { id } = params;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const task = await prisma.task.delete({
+      where: {
+        id,
+      },
+    });
+
+    return NextResponse.json(task);
+  } catch (error) {
+    console.log("ERROR DELETING TASK: ", error);
+    return NextResponse.json({ error: "Error deleting task", status: 500 });
+  }
+}
